5576eaef1f0c46f966a4218e87010814bf46ac7d
Maxim Remez <remezmaxim1992@gmail.com>
2022-08-31 11:12:09 +0300

    New task was solved 20-21

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {reverseWords} from "./Tasks/Tasks";
+
 
 function App() {
+  console.log(reverseWords("The greatest victory is that which requires no battle"))
   return (
     <div className="App">
+
     </div>
   );
 }
